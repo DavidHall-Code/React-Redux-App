7b7f56062217787053f1af3769df25716764cc5d
DavidHall-Code <david_m_hall@outlook.com>
2020-12-09 16:26:36 -0500

    setup actions index.js

---
+++ app/src/store/actions/index.js
@@ -0,0 +1,23 @@
+//API: https://api.covid19api.com/summary
+
+import axios from 'axios';
+export const FETCH_CASES_START = 'FETCH_CASES_START';
+export const FETCH_CASES_SUCCESS = 'FETCH_CASES_SUCCESS';
+export const FETCH_CASES_FAILURE = 'FETCH_CASES_FAILURE';
+
+
+
+export const fetchCases = () => {
+    return dispatch => {
+        dispatch({ type: FETCH_CASES_START })
+        axios
+            .get('https://api.covid19api.com/summary')
+            .then((res) => {
+                console.log(res)
+                dispatch({ type: FETCH_CASES_SUCCESS, payload: res.data.Countries[176]})
+            })
+            .catch((err) => {
+                console.log('There was an ERROR: ', err)
+            })
+    }
+}
