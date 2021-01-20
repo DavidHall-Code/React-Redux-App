de78322b1a3b598a9e07cf615970e4f363543845
DavidHall-Code <david_m_hall@outlook.com>
2021-01-20 16:48:29 -0500

    Setup actions folder and file

---
+++ app/src/store/actions/index.js
@@ -0,0 +1,23 @@
+//Need to import axios
+import axios from 'axios';
+//Need to export START, SUCCESS, FAILURE
+export const FETCH_CASES_START = 'FETCH_CASES_START'
+export const FETCH_CASES_SUCCESS = 'FETCH_CASES_SUCCESS'
+export const FETCH_CASES_FAILURE = 'FETCH_CASES_FAILURE'
+
+
+//Axios API `https://api.covid19api.com/summary`
+export const fetchCases = () => {
+    return dispatch => {
+        dispatch({type: FETCH_CASES_START})
+        axios
+            .get('https://api.covid19api.com/summary')
+            .then((res) => {
+                console.log(res)
+                dispatch({type: FETCH_CASES_SUCCESS, payload: res.data.Countries[176]})
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+    }
+}
